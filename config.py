463d69ed881d71d524e32cf8ff4eb6fdfacacf4e
João Víctor <93103547+JoaoVCMatias@users.noreply.github.com>
2025-09-28 18:36:13 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-# Ideal: buscar do ambiente, senão usa um fallback padrão
-SECRET_KEY = os.getenv("SECRET_KEY", "54f7f97b394be356271a54321a9260da")
-HORAS_TOKEN = 8
-DATA_BASE = "postgresql://postgres:416158@localhost:5432/lexora"
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
+HORAS_TOKEN = int(os.getenv("HORAS_TOKEN", 8))
+DATA_BASE = os.getenv("DATA_BASE", "sqlite:///local.db"
