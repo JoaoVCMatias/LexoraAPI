6c403e095c64c872f5ad8559a2f0f63c3c8c735a
João Víctor <93103547+JoaoVCMatias@users.noreply.github.com>
2025-10-10 19:00:17 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
 HORAS_TOKEN = int(os.getenv("HORAS_TOKEN", 8))
 DATA_BASE = os.getenv("DATA_BASE", "sqlite:///local.db")
-EMAIL = "lexora.lab@gmail.com"
-PASS_WORD_EMAIL = ""
+EMAIL =  os.getenv("EMAIL", "lexora.lab@gmail.com")  
+PASS_WORD_EMAIL = os.getenv("PASS_WORD_EMAIL", "")  
