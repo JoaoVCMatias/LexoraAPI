0d2b3ce8fd3e3f7dc7c95da242a89908999608bb
João Víctor <93103547+JoaoVCMatias@users.noreply.github.com>
2025-10-11 16:16:19 -0300

    Update start.sh
    Alteração para deploy no railway.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Rodando migrations..."
 alembic upgrade head   # aplica todas as migrations pendentes
 
 echo "Iniciando aplicação..."
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn main:app --host 0.0.0.0 --port 8000
