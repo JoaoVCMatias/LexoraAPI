ed8d5c2a192353f7b11c95db6333448f83bee22f
JoaoVCMatias <joaovcmatias@gmail.com>
2025-11-30 11:18:45 -0300

    correção config

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-import os
+import os 
 
-# Ideal: buscar do ambiente, senão usa um fallback padrão
-SECRET_KEY = os.getenv("SECRET_KEY", "54f7f97b394be356271a54321a9260da")
-HORAS_TOKEN = 8
-DATA_BASE = "postgresql://postgres:416158@localhost:5432/lexora"
-EMAIL = "lexora.lab@gmail.com"
-MAILTRAP_API_KEY = "7a6e0b52a76b6b93a8e04c8771941196"
-
-#"postgresql://postgres:416158@localhost:5432/lexora"
-#"postgresql://dpg-d3vndfre5dus73ahp2ag-a.oregon-postgres.render.com/dblexora_lqfv?user=lexora&password=N6HzxrTQzdOfTw9U8GQzIdxsLOSTlrH6"
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
+HORAS_TOKEN = int(os.getenv("HORAS_TOKEN", 8))
+DATA_BASE = os.getenv("DATA_BASE", "sqlite:///local.db")
+EMAIL =  os.getenv("EMAIL", "lexora.lab@gmail.com")  
+MAILTRAP_API_KEY = os.getenv("MAILTRAP_API_KEY", "")
